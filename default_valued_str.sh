3a94c01195c7b6f7521e7211c8ac8203a89bfbe2
Mogball <jeffniu22@gmail.com>
2021-10-14 23:05:38 +0000

    update

--- default_valued_str.sh
+++ default_valued_str.sh
@@ -3,16 +3,10 @@
 FILES=~/def_strattr_files
 WORKSPACE=tranform_utils
 
-g3_lookup() {
-  repl="s/files\/head\/depot/cloud\/jeffniu\/$WORKSPACE/"
-  csearch file:$1 "DefaultValuedAttr<StrAttr" --l | sed "$repl" > $FILES
-}
-
-g3_replace() {
-  cat $FILES | xargs -n 4 -P 16 perl -i -pe 's/DefaultValuedAttr<StrAttr, "(?!\\)(.*)"/DefaultValuedAttr<StrAttr, "\\"\1\\""/g'
-}
-
 replace_defstrattr() {
-  lookup='s/DefaultValuedAttr<StrAttr, "(?!\\)(.*)"'
-  grep -P "$lookup" -r $1 -l | xargs -n 4 -P 8 perl -i -pe "s/$lookup/DefaultValuedAttr<StrAttr, "\\"\1\\""/g"
+  lookup_base="<$2(<.*>)?, \"(.*)\""
+  lookup="DefaultValuedAttr$lookup_base"
+  grep -P "$lookup" -r $1 -l | xargs -n 4 -P 8 perl -i -pe "s/$lookup/DefaultValuedStrAttr<$2\1, \"\2\"/g"
+  lookup="ConstantAttr$lookup_base"
+  grep -P "$lookup" -r $1 -l | xargs -n 4 -P 8 perl -i -pe "s/$lookup/ConstantStrAttr<$2\1, \"\2\"/g"
 }
