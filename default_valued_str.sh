a767df299ed7fbfcbd2a6cc48f3f05ab457153ec
Mogball <jeffniu22@gmail.com>
2021-10-14 23:32:47 +0000

    g3 replace

--- default_valued_str.sh
+++ default_valued_str.sh
@@ -10,3 +10,12 @@ replace_defstrattr() {
   lookup="ConstantAttr$lookup_base"
   grep -P "$lookup" -r $1 -l | xargs -n 4 -P 8 perl -i -pe "s/$lookup/ConstantStrAttr<$2\1, \"\2\"/g"
 }
+
+g3_lookup() {
+  cs DefaultValuedAttr OR ConstantAttr lang:tablegen --l --f google3 | sed 's/files\/head\/depot/cloud\/jeffniu\/tranform_utils/g' > $FILES
+}
+
+g3_replace() {
+  cat $FILES | xargs -n 4 -P 8 perl -i -pe 's/DefaultValuedAttr<(.*)(<.*>)?, "\\"(.*)\\""/DefaultValuedStrAttr<\1\2, "\3"/g'
+  cat $FILES | xargs -n 4 -P 8 perl -i -pe 's/ConstantAttr<(.*)(<.*>)?, "\\"(.*)\\""/ConstantStrAttr<\1\2, "\3"/g'
+}
